0e725780af5fe30875d5d573593a4d2c0063b8dd
Joel Azemar <joel.azemar@gmail.com>
2022-12-19 22:07:12 +0100

    Initial Commit

---
+++ lib/gloom.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gloom/version"
+
+module Gloom
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gloom/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Gloom
+  VERSION = "0.1.0"
+end
